5c26986cf9ca42cf7fd3d6af5e62c12a5058bcd9
Mervin F <mervinmervin0660@gmail.com>
2026-04-28 19:05:29 +0530

    fix error

--- index.js
+++ index.js
@@ -4,25 +4,21 @@ import cors from "cors";
 import { connectDB } from "./config/db.js";
 import userRoutes from "./routes/UserRoutes.js";
 import expenseRoutes from "./routes/ExpenseRoute.js";
-// Initialize
+
 const app = express();
 dotenv.config();
 
-// Middleware
-app.use(cors({
-  origin: "*"
-}));
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 // Routes
 app.use("/users", userRoutes);
-app.use("/users/expense", expenseRoutes);
+app.use("/expense", expenseRoutes);
 
-// Connect to database and start server
 const PORT = process.env.PORT;
+
 connectDB().then(() => {
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-        
-    });
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 });
